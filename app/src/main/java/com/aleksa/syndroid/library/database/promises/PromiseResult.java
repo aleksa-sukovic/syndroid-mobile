e7fd6c111deebf48124204e49a7fe7aca9e0c208
Aleksa Sukovic <sukovic.aleksa@gmail.com>
2018-11-13 07:04:06 +0100

    Removes PromiseResult listener
    Listener is now part of Promise class, constructed as inner interface

--- app/src/main/java/com/aleksa/syndroid/library/database/promises/PromiseResult.java
+++
@@ -1,9 +0,0 @@
-package com.aleksa.syndroid.library.database.promises;
-
-import java.util.List;
-
-public interface PromiseResult<Entity>
-{
-    void itemResult(Entity data);
-    void listResult(List<Entity> data);
-}
