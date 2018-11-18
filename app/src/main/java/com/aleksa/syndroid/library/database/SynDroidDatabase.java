23d32e395aeed7e50dfbe54fb2fa49a6f0a47914
Aleksa Sukovic <sukovic.aleksa@gmail.com>
2018-11-18 19:51:24 +0100

    Removes seeds from SynDroid database class
    Seeds were used to test database, they are no longer required

--- app/src/main/java/com/aleksa/syndroid/library/database/SynDroidDatabase.java
+++ app/src/main/java/com/aleksa/syndroid/library/database/SynDroidDatabase.java
@@ -1,12 +1,9 @@
 package com.aleksa.syndroid.library.database;
 
-import android.arch.persistence.db.SupportSQLiteDatabase;
 import android.arch.persistence.room.Database;
 import android.arch.persistence.room.Room;
 import android.arch.persistence.room.RoomDatabase;
 import android.content.Context;
-import android.os.AsyncTask;
-import android.support.annotation.NonNull;
 
 import com.aleksa.syndroid.objects.server.models.Server;
 import com.aleksa.syndroid.objects.server.dao.ServerDao;
@@ -15,18 +12,11 @@ import com.aleksa.syndroid.objects.server.dao.ServerDao;
 @Database(entities = {Server.class}, version = 1)
 public abstract class SynDroidDatabase extends RoomDatabase
 {
+    // Initialization of Dao instances that are used throughout application
     public abstract ServerDao serverDao();
 
+    // Singleton implementation of SynDroid database
     private static volatile SynDroidDatabase INSTANCE;
-    private static RoomDatabase.Callback insertCallback = new RoomDatabase.Callback() {
-
-        @Override
-        public void onOpen(@NonNull SupportSQLiteDatabase db)
-        {
-            super.onOpen(db);
-            new PopulateDbAsync(INSTANCE).execute();
-        }
-    };
 
     public static SynDroidDatabase getDatabase(final Context context) {
         if (INSTANCE == null) {
@@ -34,42 +24,11 @@ public abstract class SynDroidDatabase extends RoomDatabase
                 if (INSTANCE == null) {
                     INSTANCE = Room.databaseBuilder(context.getApplicationContext(),
                         SynDroidDatabase.class, "syndroid")
-                        .addCallback(insertCallback)
                         .build();
                 }
             }
         }
 
         return INSTANCE;
-    }
-
-    private static class PopulateDbAsync extends AsyncTask<Void, Void, Void> {
-
-        private final SynDroidDatabase database;
-
-        public PopulateDbAsync(SynDroidDatabase database) {
-            this.database = database;
-        }
-
-        @Override
-        protected Void doInBackground(Void... voids)
-        {
-            database.serverDao().deleteAll();
-
-            Server[] servers = {
-                new Server("192.168.1.101", "AleksaDTC"),
-                new Server("192.168.1.102", "AleksaLPT"),
-                new Server("192.168.1.103", "Aleksa MacMini"),
-                new Server("192.168.1.104", "ZoranDTC"),
-                new Server("192.168.1.105", "MatijaLPT"),
-                new Server("192.168.1.106", "NevenaDTC"),
-            };
-
-            for (Server server: servers) {
-                database.serverDao().insert(server);
-            }
-
-            return null;
-        }
     }
 }
