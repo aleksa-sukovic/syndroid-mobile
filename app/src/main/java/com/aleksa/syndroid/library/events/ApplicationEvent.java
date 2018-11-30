d4868598fb57ad0e4117a5a5202562a8c6872e89
Aleksa Sukovic <sukovic.aleksa@gmail.com>
2018-11-30 15:34:05 +0100

    Adds ApplicationEvent class

---
+++ app/src/main/java/com/aleksa/syndroid/library/events/ApplicationEvent.java
@@ -0,0 +1,12 @@
+package com.aleksa.syndroid.library.events;
+
+public class ApplicationEvent extends Event<ApplicationEvent.EventCode>
+{
+    public ApplicationEvent(EventCode eventCode, String message) {
+        super(eventCode, message);
+    }
+
+    public enum EventCode {
+        SERVER_CREATION_ERROR, SERVER_CONNECTION_ERROR
+    }
+}
