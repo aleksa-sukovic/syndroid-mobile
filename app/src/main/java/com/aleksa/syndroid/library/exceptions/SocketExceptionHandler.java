1a531e0a26bfb4dd1405916b2d7c79db86b88747
Aleksa Sukovic <sukovic.aleksa@gmail.com>
2018-12-09 21:39:19 +0100

    Adds SocketExceptionHandler class

---
+++ app/src/main/java/com/aleksa/syndroid/library/exceptions/SocketExceptionHandler.java
@@ -0,0 +1,10 @@
+package com.aleksa.syndroid.library.exceptions;
+
+public class SocketExceptionHandler implements ExceptionHandler<BaseException, String>
+{
+    @Override
+    public String handle(BaseException s)
+    {
+        return s.render();
+    }
+}
