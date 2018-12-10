5dd026b1db3ca68bf7266a1d401c186f225a8d8d
Aleksa Sukovic <sukovic.aleksa@gmail.com>
2018-12-10 13:31:11 +0100

    Adds responseCode to Response class

--- app/src/main/java/com/aleksa/syndroid/library/router/response/Response.java
+++ app/src/main/java/com/aleksa/syndroid/library/router/response/Response.java
@@ -8,11 +8,27 @@ public class Response
 {
     private Route route;
     private Map<String, String> data;
+    private int responseCode;
 
     public Response(Route route, Map<String, String> data)
     {
         this.route = route;
         this.data = data;
+        this.responseCode = getResponseCode(data);
+    }
+
+    private int getResponseCode(Map<String,String> data)
+    {
+        String responseCodeStr = data.get("responseCode");
+        if (responseCodeStr == null) {
+            return 200;
+        }
+
+        try {
+            return Integer.parseInt(responseCodeStr);
+        } catch(Exception e) {
+            return 200;
+        }
     }
 
     public Route getRoute()
@@ -23,5 +39,10 @@ public class Response
     public Map<String, String> getData()
     {
         return data;
+    }
+
+    public int getResponseCode()
+    {
+        return this.responseCode;
     }
 }
