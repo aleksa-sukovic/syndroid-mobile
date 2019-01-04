e0b9c629382c98f73cb0a92c3df21a171ba768e2
Aleksa Sukovic <sukovic.aleksa@gmail.com>
2019-01-04 21:54:05 +0100

    Adds fragments classes

---
+++ app/src/main/java/com/aleksa/syndroid/fragments/mouse/MouseFragment.java
@@ -0,0 +1,21 @@
+package com.aleksa.syndroid.fragments.mouse;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.aleksa.syndroid.R;
+
+public class MouseFragment extends Fragment
+{
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState)
+    {
+        return inflater.inflate(R.layout.fragment_mouse, container, false);
+    }
+}
